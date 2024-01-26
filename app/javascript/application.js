3afc184890ef0d91a21d5ef09f106717dffd3b46
EC2 Default User <ec2-user@ip-172-31-7-169.ap-northeast-1.compute.internal>
2024-01-26 04:04:21 +0000

    fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,44 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import jquery from "jquery"
 window.$ = jquery
-import "./controllers/image_preview_controller.js";
+import "./controllers/image_preview_controller";
+
+import { Controller } from "@hotwired/stimulus"
+
+let data = null;
+
+export default class extends Controller {
+  static targets = [ "output", "input"];
+  
+  connect() {
+    data = new DataTransfer();
+  }
+
+  readImage(e) {
+    let input = this.inputTarget;
+    let output = this.outputTarget;
+
+    if (input.files) {
+      let files = Array.from(input.files);
+      
+      files.forEach((f) => data.items.add(f));
+      for (let i = 0; i < files.length; i++) {
+        this.readOne(files[i], i, output);
+      }
+      input.files = data.files;
+    }
+  }
+
+  readOne(f, index, output) {
+    let reader = new FileReader();
+    reader.readAsDataURL(f);
+    reader.onload = () => {
+      const html= `<li>
+                  <div>
+                    <image src="${reader.result}" class='movie-png'/>
+                  </div>
+                </li>`;
+      output.insertAdjacentHTML('beforeend', html);
+    };
+  }
+}
